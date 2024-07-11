e190bf319dfa56daa68234368990cfaf0a083248
incognito12345 <84151800+incognito12345@users.noreply.github.com>
2024-07-11 17:40:49 +0100

    Update Main.js

--- BendingSeries/Main.js
+++ BendingSeries/Main.js
@@ -2,3 +2,7 @@ function openNav() {
 	document.getElementById("mySidenav").style.width = "250px";
 	document.getElementById("main").style.marginLeft = "250px";
 }
+function closeNav() {
+	document.getElementById("mySidenav").style.width = "0";
+	document.getElementById("main").style.marginLeft = "0";
+}
